d39f199d62814b3ee49dcc29fdfd96f1c0b769cf
kimkanovsky <kimkanovsky@yandex.ru>
2021-11-16 18:58:52 +0300

    second9

--- api/filters.py
+++ api/filters.py
@@ -1,10 +1,10 @@
-from django_filters import CharFilter
-from django_filters import rest_framework as filters
+from django_filters.filters import CharFilter
+from django_filters.rest_framework.filterset import FilterSet
 
 from .models import Title
 
 
-class TitleFilter(filters.filterset.FilterSet):
+class TitleFilter(FilterSet):
     name = CharFilter(lookup_expr='icontains')
     category = CharFilter(field_name='category__slug')
     genre = CharFilter(field_name='genre__slug')
